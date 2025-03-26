92e8200f1d0f00775a195c6d42e23438f8c12123
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-03-26 10:03:31 -0400

    setting up arch-clint.ads

--- source/arch/riscv64-limine/arch-clint.ads
+++ source/arch/riscv64-limine/arch-clint.ads
@@ -39,14 +39,51 @@ package Arch.CLINT is
    --  A Boolean flag (Enabled) indicates whether the CLINT is supported. When
    --  disabled, all functions report the lack of support and return safe defaults.
    ------------------------------------------------------------------------------
-   type CLINT_Registers is record
-      MSIP : Unsigned_32;
-      Reserved1 : Unsigned_32;
-      MTIMECMP : Unsigned_64;
-      MTIMECMP_H : Unsigned_32;
-      Reserved2 : Unsigned_32;
-      MTIME : Unsigned_64;
-      MTIME_H : Unsigned_32;
-      Reserved3 : Unsigned_32;
-   end record;
+   with System;
+with Interfaces; use Interfaces;
+
+package Arch.CLINT with SPARK_Mode => Off is
+   ------------------------------------------------------------------------------
+   --  Arch.CLINT
+   --
+   --  This package encapsulates the Core Local Interruptor (CLINT) for RISCV64.
+   --  All configuration parameters (other than those defined by the RISCV64 ISA)
+   --  are set dynamically via Set_CLINT_Configuration. A Boolean flag (Enabled)
+   --  indicates whether the CLINT is supported on the platform.
+   ------------------------------------------------------------------------------
+   procedure Set_CLINT_Configuration (
+     Base_Address     : System.Address := System'To_Address(16#02000000#);
+     MSIP_Offset      : Unsigned_64   := 0;
+     MTime_Offset     : Unsigned_64   := 16#BFF8#;
+     MTimecmp_Offset  : Unsigned_64   := 16#4000#;
+     Enabled          : Boolean       := True
+   )
+   with Pre  => True,
+        Post => (Get_CLINT_Base = Base_Address) and then 
+                (Get_MSIP_Offset = MSIP_Offset) and then 
+                (Get_MTime_Offset = MTime_Offset) and then 
+                (Get_MTimecmp_Offset = MTimecmp_Offset) and then 
+                (CLINT_Enabled = Enabled);
+   
+   function Get_CLINT_Base return System.Address;
+   function Get_MSIP_Offset return Unsigned_64;
+   function Get_MTime_Offset return Unsigned_64;
+   function Get_MTimecmp_Offset return Unsigned_64;
+   function CLINT_Enabled return Boolean;
+
+   ------------------------------------------------------------------------------
+   --  Software Interrupt Management
+   ------------------------------------------------------------------------------
+   procedure Set_Software_Interrupt (Hart_ID : Unsigned_64; Value : Boolean)
+      with Inline,
+           Pre  => Hart_ID >= 0,
+           Post => True;
+   procedure Clear_Software_Interrupt (Hart_ID : Unsigned_64)
+      with Inline,
+           Pre  => Hart_ID >= 0,
+           Post => True;
+   function Read_Software_Interrupt (Hart_ID : Unsigned_64) return Boolean
+      with Inline,
+           Pre  => Hart_ID >= 0,
+           Post => True;
    
