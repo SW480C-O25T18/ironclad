35052a036205ace44528f1afe784f5e5a5d0ebca
Christopher <fartdumpster@gmail.com>
2025-04-01 21:25:12 -0400

    Commented and restart :)

--- source/arch/riscv64-limine/arch-entrypoint.adb
+++ source/arch/riscv64-limine/arch-entrypoint.adb
@@ -14,6 +14,198 @@
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+-- with Interfaces; use Interfaces;
+-- with Devices.UART;
+-- with Arch.Limine;
+-- with Lib.Messages; use Lib.Messages;
+-- with Memory.Physical;
+-- with Arch.MMU;
+-- with Lib.Panic;
+--       
+-- with Arch.DTB;
+-- with Arch.CPU;
+-- with Main;
+-- with Arch.Power;
+
+-- #if KASAN
+--    with Lib.KASAN;
+-- #end if;
+
+-- package body Arch.Entrypoint is
+ --  procedure Bootstrap_Main is
+      -- Info : Boot_Information renames Limine.Global_Info;
+      -- Addr : System.Address;
+  -- begin
+      --  Initialize architectural state first.
+      -- Devices.UART.Init_UART0;
+
+      -- Lib.Messages.Put_Line ("Hello");
+
+      -- Commenting out the Reboot procedure call and its handling
+      -- Arch.Power.Reboot(Status);
+
+      -- if Status = Failure then
+      --    Lib.Messages.Put_Line ("Reboot failed.");
+      -- else
+      --    Lib.Messages.Put_Line ("Reboot succeeded.");
+      -- end if;
+
+      -- loop null; end loop;
+
+      --  Translate the limine protocol into arch-agnostic structures.
+      -- Limine.Translate_Proto;
+
+      --  Initialize device discovery facilities.
+      -- if not Arch.DTB.Init then
+      --    Lib.Panic.Hard_Panic ("No DTB was found!");
+      -- end if;
+
+      --  Initialize the allocators and MMU.
+      -- Lib.Messages.Put_Line ("Initializing allocators");
+      -- Memory.Physical.Init_Allocator (Info.Memmap (1 .. Info.Memmap_Len));
+      -- if not Arch.MMU.Init (Info.Memmap (1 .. Info.Memmap_Len)) then
+      --    Lib.Panic.Hard_Panic ("The VMM could not be initialized");
+      -- end if;
+
+      --  Enable dmesg buffers and KASAN if wanted.
+      -- Lib.Messages.Enable_Logging;
+      -- #if KASAN
+      --    Lib.KASAN.Init;
+      -- #end if;
+
+      --  Print the memory map, it is useful at times.
+      -- Lib.Messages.Put_Line ("Physical memory map:");
+      -- for E of Info.Memmap (1 .. Info.Memmap_Len) loop
+      --    Addr := E.Start + E.Length;
+      --    Lib.Messages.Put_Line
+      --       ("[" & E.Start'Image & " - " & Addr'Image & "] " &
+      --        Boot_Memory_Type'Image (E.MemType));
+      -- end loop;
+
+      --  Initialize the other cores of the system.
+      -- Arch.CPU.Init_Cores;
+
+      --  Go to main kernel.
+      -- Arch.Cmdline_Len := Info.Cmdline_Len;
+      -- Arch.Cmdline (1 .. Info.Cmdline_Len) :=
+      --    Info.Cmdline (1 .. Info.Cmdline_Len);
+
+      -- Main;
+
+--  arch-entrypoint.adb: Limine plops us here.
+--  Copyright (C) 2024 streaksu
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Arch.Debug;
+with Interfaces; use Interfaces;
+with Devices.UART;
+with Arch.Limine;
+with Lib.Messages; use Lib.Messages;
+with Memory.Physical;
+with Arch.MMU;
+with Lib.Panic;
+with Arch.DTB;
+with Arch.CPU;
+with Main;
+
+#if KASAN
+ with Lib.KASAN;
+#end if;
+
+package body Arch.Entrypoint is
+   procedure Bootstrap_Main is
+      Info : Boot_Information renames Limine.Global_Info;
+      Addr : System.Address;
+   begin
+      --  Initialize architectural state first.
+      Devices.UART.Init_UART0;
+
+      Lib.Messages.Put_Line ("Hello");
+
+--      loop null; end loop;
+
+      --  Translate the limine protocol into arch-agnostic structures.
+      Limine.Translate_Proto;
+
+      --  Initialize device discovery facilities.
+      Debug.Print ("Initializing DTB discovery");
+      if not Arch.DTB.Init then
+         Lib.Panic.Hard_Panic ("No DTB was found!");
+      end if;
+
+      --  Initialize the allocators and MMU.
+      Debug.Print ("Initializing allocators and MMU");
+      Lib.Messages.Put_Line ("Initializing allocators");
+      Memory.Physical.Init_Allocator (Info.Memmap (1 .. Info.Memmap_Len));
+      Debug.Print ("Physical allocator initialized");
+      Debug.Print ("Initializing MMU");
+      if not Arch.MMU.Init (Info.Memmap (1 .. Info.Memmap_Len)) then
+         Lib.Panic.Hard_Panic ("The VMM could not be initialized");
+      end if;
+      Debug.Print ("MMU initialized");
+
+      --  Enable dmesg buffers and KASAN if wanted.
+      Debug.Print ("Enabling logging");
+      Lib.Messages.Enable_Logging;
+      Debug.Print ("Logging enabled");
+      --pragma Compile_Time_Error
+       -- (Condition => KASAN_Supported,
+       --  Message => "KASAN is not supported in this build");
+      Debug.Print ("Initializing KASAN");
+      Lib.KASAN.Init;
+      Debug.Print ("KASAN initialized");
+
+      --  Print the memory map, it is useful at times.
+      Lib.Messages.Put_Line ("Physical memory map:");
+      for E of Info.Memmap (1 .. Info.Memmap_Len) loop
+         Addr := E.Start + E.Length;
+         Lib.Messages.Put_Line
+            ("[" & E.Start'Image & " - " & Addr'Image & "] " &
+             Boot_Memory_Type'Image (E.MemType));
+      end loop;
+
+      --  Initialize the other cores of the system.
+      Debug.Print ("Initializing cores");
+      Arch.CPU.Init_Cores;
+      Debug.Print ("Cores initialized");
+
+      --  Go to main kernel.
+      Debug.Print ("Copying command line");
+      Arch.Cmdline_Len := Info.Cmdline_Len;
+      Arch.Cmdline (1 .. Info.Cmdline_Len) :=
+         Info.Cmdline (1 .. Info.Cmdline_Len);
+      Debug.Print ("Command line copied");
+      Debug.Print ("Jumping to main kernel");
+      Main;
+   end Bootstrap_Main;
+end Arch.Entrypoint;--  arch-entrypoint.adb: Limine plops us here.
+--  Copyright (C) 2024 streaksu
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 with Interfaces; use Interfaces;
 with Devices.UART;
 with Arch.Limine;
--- source/arch/riscv64-limine/arch-power.adb
+++ source/arch/riscv64-limine/arch-power.adb
@@ -1,5 +1,111 @@
--- filepath: /home/crew18/Gloire/ironclad/source/arch/riscv64-limine/arch-power.adb
---  arch-power.adb: Architecture-specific power management for RISC-V with Limine.
+--  -- filepath: /home/crew18/Gloire/ironclad/source/arch/riscv64-limine/arch-power.adb
+--  --  arch-power.adb: Architecture-specific power management for RISC-V with Limine.
+--  --  Copyright (C) 2025 streaksu
+--  --
+--  --  This program is free software: you can redistribute it and/or modify
+--  --  it under the terms of the GNU General Public License as published by
+--  --  the Free Software Foundation, either version 3 of the License, or
+--  --  (at your option) any later version.
+--  --
+--  --  This program is distributed in the hope that it will be useful,
+--  --  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  --  GNU General Public License for more details.
+--  --
+--  --  You should have received a copy of the GNU General Public License
+--  --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--  with Arch.Snippets;
+--  with Lib.Panic;
+--  with SBI;  -- Include SBI interface for system reset
+--  with Ada.Text_IO;  -- For terminal output
+
+--  package body Arch.Power is
+
+--     procedure Halt (Status : out Power_Status) is
+--     begin
+--        --  Halt the system by entering an infinite loop.
+--        Lib.Panic.Hard_Panic ("System halted.");
+--        Ada.Text_IO.Put_Line ("Halt procedure called: System is halting.");
+
+--        -- Uncomment the following line to simulate a failure state for testing.
+--        -- Status := Failure;
+--        -- Ada.Text_IO.Put_Line ("Halt procedure: Simulated failure state.");
+--        -- return;
+
+--        loop
+--           null;
+--        end loop;
+
+--        --  If somehow we exit the loop, set the status to failure.
+--        Status := Failure;
+--        lib.Messages.Put_Line ("Halt procedure: Exited loop unexpectedly. Status set to Failure.");
+--     end Halt;
+
+--     procedure Reboot (Status : out Power_Status) is
+--        Result : Sbiret;
+--     begin
+--        lib.Messages.Put_Line ("Reboot procedure called: Attempting to reboot.");
+      
+--        loop 
+--           null;
+         
+--        end loop;
+--        -- Uncomment the following line to simulate a failure state for testing.
+--        -- Status := Failure;
+--        -- Ada.Text_IO.Put_Line ("Reboot procedure: Simulated failure state.");
+--        -- return;
+
+--        --  Perform a cold reboot using SBI system reset.
+--        Result := SBI.System_Reset (16#00000001#, 16#00000000#); -- Cold reboot, no reason
+
+--        --  Check for errors in the SBI response
+--        if Result.Error /= 1 then
+--           Status := Failure;
+--           Ada.Text_IO.Put_Line ("Reboot procedure: SBI call failed. Status set to Failure.");
+--        else
+--           -- Uncomment the following line to force a success state for testing.
+--           -- Status := Success;
+--           -- Ada.Text_IO.Put_Line ("Reboot procedure: Simulated success state.");
+--           -- return;
+
+--           -- This point should never be reached if reboot succeeds
+--           Status := Success;
+--           lib.Messages.Put_Line ("Reboot procedure: SBI call succeeded. Status set to Success.");
+--        end if;
+--    end Reboot;
+
+--     procedure Poweroff (Status : out Power_Status) is
+--        Result : Sbiret;
+--     begin
+--        Ada.Text_IO.Put_Line ("Poweroff procedure called: Attempting to power off.");
+
+--        -- Uncomment the following line to simulate a failure state for testing.
+--        -- Status := Failure;
+--        -- Ada.Text_IO.Put_Line ("Poweroff procedure: Simulated failure state.");
+--        -- return;
+
+--        --  Attempt to power off using SBI system reset.
+--        Result := SBI.System_Reset (16#00000000#, 16#00000000#); -- Shutdown, no reason
+
+--        --  Check for errors in the SBI response
+--        if Result.Error /= 0 then
+--           Status := Failure;
+--           Ada.Text_IO.Put_Line ("Poweroff procedure: SBI call failed. Status set to Failure.");
+--        else
+--           -- Uncomment the following line to force a success state for testing.
+--           -- Status := Success;
+--           -- Ada.Text_IO.Put_Line ("Poweroff procedure: Simulated success state.");
+--           -- return;
+
+--           -- This point should never be reached if poweroff succeeds
+--           Status := Success;
+--           Ada.Text_IO.Put_Line ("Poweroff procedure: SBI call succeeded. Status set to Success.");
+--        end if;
+--     end Poweroff;
+
+--  end Arch.Power;
+--  arch-power.adb: Architecture-specific power management.
 --  Copyright (C) 2025 streaksu
 --
 --  This program is free software: you can redistribute it and/or modify
@@ -15,89 +121,19 @@
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Arch.Snippets;
-with Lib.Panic;
-with SBI;  -- Include SBI interface for system reset
-with Ada.Text_IO;  -- For terminal output
-
 package body Arch.Power is
-
    procedure Halt (Status : out Power_Status) is
    begin
-      --  Halt the system by entering an infinite loop.
-      Lib.Panic.Hard_Panic ("System halted.");
-      Ada.Text_IO.Put_Line ("Halt procedure called: System is halting.");
-
-      -- Uncomment the following line to simulate a failure state for testing.
-      -- Status := Failure;
-      -- Ada.Text_IO.Put_Line ("Halt procedure: Simulated failure state.");
-      -- return;
-
-      loop
-         null;
-      end loop;
-
-      --  If somehow we exit the loop, set the status to failure.
       Status := Failure;
-      Ada.Text_IO.Put_Line ("Halt procedure: Exited loop unexpectedly. Status set to Failure.");
    end Halt;
 
    procedure Reboot (Status : out Power_Status) is
-      Result : Sbiret;
    begin
-      Ada.Text_IO.Put_Line ("Reboot procedure called: Attempting to reboot.");
-
-      -- Uncomment the following line to simulate a failure state for testing.
-      -- Status := Failure;
-      -- Ada.Text_IO.Put_Line ("Reboot procedure: Simulated failure state.");
-      -- return;
-
-      --  Perform a cold reboot using SBI system reset.
-      Result := SBI.System_Reset (16#00000001#, 16#00000000#); -- Cold reboot, no reason
-
-      --  Check for errors in the SBI response
-      if Result.Error /= 0 then
-         Status := Failure;
-         Ada.Text_IO.Put_Line ("Reboot procedure: SBI call failed. Status set to Failure.");
-      else
-         -- Uncomment the following line to force a success state for testing.
-         -- Status := Success;
-         -- Ada.Text_IO.Put_Line ("Reboot procedure: Simulated success state.");
-         -- return;
-
-         -- This point should never be reached if reboot succeeds
-         Status := Success;
-         Ada.Text_IO.Put_Line ("Reboot procedure: SBI call succeeded. Status set to Success.");
-      end if;
+      Status := Failure;
    end Reboot;
 
    procedure Poweroff (Status : out Power_Status) is
-      Result : Sbiret;
    begin
-      Ada.Text_IO.Put_Line ("Poweroff procedure called: Attempting to power off.");
-
-      -- Uncomment the following line to simulate a failure state for testing.
-      -- Status := Failure;
-      -- Ada.Text_IO.Put_Line ("Poweroff procedure: Simulated failure state.");
-      -- return;
-
-      --  Attempt to power off using SBI system reset.
-      Result := SBI.System_Reset (16#00000000#, 16#00000000#); -- Shutdown, no reason
-
-      --  Check for errors in the SBI response
-      if Result.Error /= 0 then
-         Status := Failure;
-         Ada.Text_IO.Put_Line ("Poweroff procedure: SBI call failed. Status set to Failure.");
-      else
-         -- Uncomment the following line to force a success state for testing.
-         -- Status := Success;
-         -- Ada.Text_IO.Put_Line ("Poweroff procedure: Simulated success state.");
-         -- return;
-
-         -- This point should never be reached if poweroff succeeds
-         Status := Success;
-         Ada.Text_IO.Put_Line ("Poweroff procedure: SBI call succeeded. Status set to Success.");
-      end if;
+      Status := Failure;
    end Poweroff;
-
 end Arch.Power;
