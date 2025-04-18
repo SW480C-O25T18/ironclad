e5ab9c4b6c9ab6db0814c6c273f8030d53b0b11b
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-04-18 15:28:04 -0400

    suppressing warnings from arch-clocks.adb

--- source/arch/riscv64-limine/arch-clocks.adb
+++ source/arch/riscv64-limine/arch-clocks.adb
@@ -14,6 +14,8 @@
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+pragma Warnings (Off);
+
 package body Arch.Clocks with
    Refined_State =>
       (RT_Clock_State =>
