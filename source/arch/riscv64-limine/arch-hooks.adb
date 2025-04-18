c3da37430d804899e2ef21f30962919ddd5e55c1
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-04-18 14:17:33 -0400

    fixing more issues.

--- source/arch/riscv64-limine/arch-hooks.adb
+++ source/arch/riscv64-limine/arch-hooks.adb
@@ -1,6 +1,6 @@
--- filepath: /home/sean/capstone/ironclad/source/arch/riscv64-limine/arch-hooks.adb
 --  arch-hooks.adb: Architecture-specific hooks for several utilities.
 --  Copyright (C) 2024 streaksu
+--  Copyright (C) 2025 scweeks
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
