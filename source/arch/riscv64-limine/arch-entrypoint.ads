c5f4b5bca619f014ac9d747d06068e9ccfadcde6
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-04-18 20:52:26 -0400

    Added "use Interfaces" to arch-entrypoint.ads

--- source/arch/riscv64-limine/arch-entrypoint.ads
+++ source/arch/riscv64-limine/arch-entrypoint.ads
@@ -14,7 +14,7 @@
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Interfaces;
+with Interfaces; use Interfaces;
 
 package Arch.Entrypoint is
    --  Convert Unsigned_64 to String using Ada.Strings.Fixed
