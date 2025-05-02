b30f827bce96f27a4fc92efa10d269e12d2afedb
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-05-01 23:02:42 -0400

    still fixing errors in clint.

--- source/arch/riscv64-limine/arch-clint.adb
+++ source/arch/riscv64-limine/arch-clint.adb
@@ -1,4 +1,4 @@
---  arch-clint.adb: Specification of Core Local Interruptor (CLINT) utilities.
+--  arch-clint.adb: Implementation of Core Local Interruptor (CLINT) utilities.
 --  Copyright (C) 2025 Sean C. Weeks - badrock1983
 --
 --  This program is free software: you can redistribute it and/or modify
@@ -19,33 +19,23 @@ with System; -- For System.Address and related utilities
 with Arch; -- For accessing CLINT_State and other architecture-specific definitions
 with Arch.Debug; -- For debug printing
 with Ada.Unchecked_Conversion; -- For Address and Unsigned_64 conversions
-with Ada.Assertions; -- For runtime assertions
 
 package body Arch.CLINT is
 
-   -----------------------------------------------------------------------------
-   --  Address Conversions
-   -----------------------------------------------------------------------------
-   function Address_To_U64 is new Ada.Unchecked_Conversion(
-      Source => System.Address,
-      Target => Interfaces.Unsigned_64
-   );
+   --  Address conversions.
+   function Address_To_U64 is new Ada.Unchecked_Conversion
+     (Source => System.Address, Target => Unsigned_64);
 
-   function U64_To_Address is new Ada.Unchecked_Conversion(
-      Source => Interfaces.Unsigned_64,
-      Target => System.Address
-   );
+   function U64_To_Address is new Ada.Unchecked_Conversion
+     (Source => Unsigned_64, Target => System.Address);
 
-   -----------------------------------------------------------------------------
-   --  Configuration API
-   -----------------------------------------------------------------------------
-   procedure Set_CLINT_Configuration (
-      Base_Address     : System.Address := System'To_Address(16#02000000#);
-      MSIP_Offset      : Interfaces.Unsigned_64 := 0;
-      MTime_Offset     : Interfaces.Unsigned_64 := 16#BFF8#;
-      MTimecmp_Offset  : Interfaces.Unsigned_64 := 16#4000#;
-      Enabled          : Boolean := True
-   ) is
+   --  Configuration API.
+   procedure Set_CLINT_Configuration
+     (Base_Address     : System.Address := System'To_Address(16#02000000#);
+      MSIP_Offset      : Unsigned_64    := 0;
+      MTime_Offset     : Unsigned_64    := 16#BFF8#;
+      MTimecmp_Offset  : Unsigned_64    := 16#4000#;
+      Enabled          : Boolean        := True) is
    begin
       CLINT_State.Base_Address    := Base_Address;
       CLINT_State.MSIP_Offset     := MSIP_Offset;
@@ -54,10 +44,6 @@ package body Arch.CLINT is
       CLINT_State.Enabled         := Enabled;
 
       Arch.Debug.Print("CLINT: Configuration updated");
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred during configuration");
-         raise;
    end Set_CLINT_Configuration;
 
    function Get_CLINT_Base return System.Address is
@@ -65,17 +51,17 @@ package body Arch.CLINT is
       return CLINT_State.Base_Address;
    end Get_CLINT_Base;
 
-   function Get_MSIP_Offset return Interfaces.Unsigned_64 is
+   function Get_MSIP_Offset return Unsigned_64 is
    begin
       return CLINT_State.MSIP_Offset;
    end Get_MSIP_Offset;
 
-   function Get_MTime_Offset return Interfaces.Unsigned_64 is
+   function Get_MTime_Offset return Unsigned_64 is
    begin
       return CLINT_State.MTime_Offset;
    end Get_MTime_Offset;
 
-   function Get_MTimecmp_Offset return Interfaces.Unsigned_64 is
+   function Get_MTimecmp_Offset return Unsigned_64 is
    begin
       return CLINT_State.MTimecmp_Offset;
    end Get_MTimecmp_Offset;
@@ -85,150 +71,80 @@ package body Arch.CLINT is
       return CLINT_State.Enabled;
    end CLINT_Enabled;
 
-   -----------------------------------------------------------------------------
-   --  MMIO Access
-   -----------------------------------------------------------------------------
-   type Reg_Type is new Interfaces.Unsigned_64;
+   --  MMIO Access.
+   type Reg_Type is new Unsigned_64;
    pragma Volatile (Reg_Type);
    type Reg_Ptr is access all Reg_Type;
 
-   function To_Reg_Ptr is new Ada.Unchecked_Conversion(
-      Source => System.Address,
-      Target => Reg_Ptr
-   );
+   function To_Reg_Ptr is new Ada.Unchecked_Conversion
+     (Source => System.Address, Target => Reg_Ptr);
 
    function Reg (Addr : System.Address) return Reg_Ptr is
    begin
-      if Addr = System.Null_Address then
-         Arch.Debug.Print("CLINT: Null address passed to Reg function");
-         raise Program_Error with "Null address passed to Reg function";
-      end if;
       return To_Reg_Ptr(Addr);
    end Reg;
 
-   -----------------------------------------------------------------------------
-   --  Software Interrupts
-   -----------------------------------------------------------------------------
-   procedure Set_Software_Interrupt(Hart_ID : Interfaces.Unsigned_64; Value : Boolean) is
+   --  Software Interrupts.
+   procedure Set_Software_Interrupt (Hart_ID : Unsigned_64; Value : Boolean) is
       Addr : constant System.Address := U64_To_Address(
-         Address_To_U64(Get_CLINT_Base) + Get_MSIP_Offset + Hart_ID * 4
-      );
+        Address_To_U64(Get_CLINT_Base) + Get_MSIP_Offset + Hart_ID * 4);
       R : Reg_Ptr := Reg(Addr);
    begin
-      if not CLINT_Enabled then
-         Arch.Debug.Print("CLINT: Attempted to set software interrupt while CLINT is disabled");
-         raise Program_Error with "CLINT is disabled";
-      end if;
-
       if Value then
          R.all := 1;
       else
          R.all := 0;
       end if;
 
-      Arch.Debug.Print("CLINT: Software interrupt set for Hart_ID = " & Interfaces.Unsigned_64'Image(Hart_ID));
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while setting software interrupt");
-         raise;
+      Arch.Debug.Print("CLINT: Software interrupt set for Hart_ID = " &
+        Unsigned_64'Image(Hart_ID));
    end Set_Software_Interrupt;
 
-   procedure Clear_Software_Interrupt(Hart_ID : Interfaces.Unsigned_64) is
+   procedure Clear_Software_Interrupt (Hart_ID : Unsigned_64) is
    begin
       Set_Software_Interrupt(Hart_ID, False);
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while clearing software interrupt");
-         raise;
    end Clear_Software_Interrupt;
 
-   function Read_Software_Interrupt(Hart_ID : Interfaces.Unsigned_64) return Boolean is
+   function Read_Software_Interrupt (Hart_ID : Unsigned_64) return Boolean is
       Addr : constant System.Address := U64_To_Address(
-         Address_To_U64(Get_CLINT_Base) + Get_MSIP_Offset + Hart_ID * 4
-      );
+        Address_To_U64(Get_CLINT_Base) + Get_MSIP_Offset + Hart_ID * 4);
       R : Reg_Ptr := Reg(Addr);
    begin
-      if not CLINT_Enabled then
-         Arch.Debug.Print("CLINT: Attempted to read software interrupt while CLINT is disabled");
-         raise Program_Error with "CLINT is disabled";
-      end if;
-
       return R.all /= 0;
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while reading software interrupt");
-         raise;
    end Read_Software_Interrupt;
 
-   -----------------------------------------------------------------------------
-   --  Timer
-   -----------------------------------------------------------------------------
-   function Get_MTime return Interfaces.Unsigned_64 is
+   --  Timer.
+   function Get_MTime return Unsigned_64 is
       Addr : constant System.Address := U64_To_Address(
-         Address_To_U64(Get_CLINT_Base) + Get_MTime_Offset
-      );
+        Address_To_U64(Get_CLINT_Base) + Get_MTime_Offset);
       R : Reg_Ptr := Reg(Addr);
    begin
-      if not CLINT_Enabled then
-         Arch.Debug.Print("CLINT: Attempted to get mtime while CLINT is disabled");
-         raise Program_Error with "CLINT is disabled";
-      end if;
-
       return R.all;
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while getting mtime");
-         raise;
    end Get_MTime;
 
-   procedure Set_Timer_Compare(Hart_ID : Interfaces.Unsigned_64; Time : Interfaces.Unsigned_64) is
+   procedure Set_Timer_Compare (Hart_ID : Unsigned_64; Time : Unsigned_64) is
       Addr : constant System.Address := U64_To_Address(
-         Address_To_U64(Get_CLINT_Base) + Get_MTimecmp_Offset + Hart_ID * 8
-      );
+        Address_To_U64(Get_CLINT_Base) + Get_MTimecmp_Offset + Hart_ID * 8);
       R : Reg_Ptr := Reg(Addr);
    begin
-      if not CLINT_Enabled then
-         Arch.Debug.Print("CLINT: Attempted to set timer compare while CLINT is disabled");
-         raise Program_Error with "CLINT is disabled";
-      end if;
-
       R.all := Time;
-      Arch.Debug.Print("CLINT: Timer compare set for Hart_ID = " & Interfaces.Unsigned_64'Image(Hart_ID));
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while setting timer compare");
-         raise;
+      Arch.Debug.Print("CLINT: Timer compare set for Hart_ID = " &
+        Unsigned_64'Image(Hart_ID));
    end Set_Timer_Compare;
 
-   function Get_Timer_Compare(Hart_ID : Interfaces.Unsigned_64) return Interfaces.Unsigned_64 is
+   function Get_Timer_Compare (Hart_ID : Unsigned_64) return Unsigned_64 is
       Addr : constant System.Address := U64_To_Address(
-         Address_To_U64(Get_CLINT_Base) + Get_MTimecmp_Offset + Hart_ID * 8
-      );
+        Address_To_U64(Get_CLINT_Base) + Get_MTimecmp_Offset + Hart_ID * 8);
       R : Reg_Ptr := Reg(Addr);
    begin
-      if not CLINT_Enabled then
-         Arch.Debug.Print("CLINT: Attempted to get timer compare while CLINT is disabled");
-         raise Program_Error with "CLINT is disabled";
-      end if;
-
       return R.all;
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while getting timer compare");
-         raise;
    end Get_Timer_Compare;
 
-   -----------------------------------------------------------------------------
-   --  Memory Barrier
-   -----------------------------------------------------------------------------
+   --  Memory Barrier.
    procedure Memory_Barrier is
    begin
       Asm ("fence", Volatile => True, Clobber => "memory");
       Arch.Debug.Print("CLINT: Memory barrier executed");
-   exception
-      when others =>
-         Arch.Debug.Print("CLINT: Error occurred while executing memory barrier");
-         raise;
    end Memory_Barrier;
 
 end Arch.CLINT;
--- source/arch/riscv64-limine/arch-clint.ads
+++ source/arch/riscv64-limine/arch-clint.ads
@@ -1,4 +1,4 @@
---  arch-exceptions.ads: Specification of Core Local Interruptor (CLINT) utilities.
+--  arch-clint.ads: Specification of Core Local Interruptor (CLINT) utilities.
 --  Copyright (C) 2025 Sean C. Weeks - badrock1983
 --
 --  This program is free software: you can redistribute it and/or modify
@@ -12,95 +12,38 @@
 --  GNU General Public License for more details.
 --
 --  You should have received a copy of the GNU General Public License
---  along with this program.  If not, see <http://www.gnu.
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 with System; use System;
 with Interfaces; use Interfaces;
 
-package Arch.CLINT with SPARK_Mode => Off is
-   ------------------------------------------------------------------------------
-   --  Arch.CLINT
-   --
-   --  This package encapsulates the Core Local Interruptor (CLINT) for RISCV64.
-   --  It supports dynamic configuration based on DTB (or other boot configuration)
-   --  parameters. All configuration parameters not dictated by the RISCV64 ISA are
-   --  set via Set_CLINT_Configuration. Default values are provided to allow basic
-   --  functionality even if DTB parsing is incomplete.
-   --
-   --  The CLINT manages:
-   --    - Software interrupts (msip registers) for each hart:
-   --         Address = CLINT_Base + MSIP_Offset + (Hart_ID * 4)
-   --
-   --    - Timer registers:
-   --         mtime (global, 64-bit) at CLINT_Base + MTime_Offset
-   --         mtimecmp (per hart, 64-bit) at
-   --             CLINT_Base + MTimecmp_Offset + (Hart_ID * 8)
-   --
-   --  A Boolean flag (Enabled) indicates whether the CLINT is supported. When
-   --  disabled, all functions report the lack of support and return safe defaults.
-   ------------------------------------------------------------------------------
+package Arch.CLINT is
+   --  Set CLINT configuration dynamically.
+   procedure Set_CLINT_Configuration
+     (Base_Address     : System.Address := System'To_Address(16#02000000#);
+      MSIP_Offset      : Unsigned_64    := 0;
+      MTime_Offset     : Unsigned_64    := 16#BFF8#;
+      MTimecmp_Offset  : Unsigned_64    := 16#4000#;
+      Enabled          : Boolean        := True);
 
-   
-
-   procedure Set_CLINT_Configuration (
-     Base_Address     : System.Address := System'To_Address(16#02000000#);
-     MSIP_Offset      : Unsigned_64   := 0;
-     MTime_Offset     : Unsigned_64   := 16#BFF8#;
-     MTimecmp_Offset  : Unsigned_64   := 16#4000#;
-     Enabled          : Boolean       := True
-   )
-   with Pre  => True,
-        Post => (Get_CLINT_Base = Base_Address) and then 
-                (Get_MSIP_Offset = MSIP_Offset) and then 
-                (Get_MTime_Offset = MTime_Offset) and then 
-                (Get_MTimecmp_Offset = MTimecmp_Offset) and then 
-                (CLINT_Enabled = Enabled);
-   
+   --  Getters for CLINT configuration.
    function Get_CLINT_Base return System.Address;
    function Get_MSIP_Offset return Unsigned_64;
    function Get_MTime_Offset return Unsigned_64;
    function Get_MTimecmp_Offset return Unsigned_64;
    function CLINT_Enabled return Boolean;
 
-   ------------------------------------------------------------------------------
-   --  Software Interrupt Management
-   ------------------------------------------------------------------------------
-   procedure Set_Software_Interrupt (Hart_ID : Unsigned_64; Value : Boolean)
-      with Inline,
-           Pre  => Hart_ID >= 0,
-           Post => True;
+   --  Software interrupt management.
+   procedure Set_Software_Interrupt (Hart_ID : Unsigned_64; Value : Boolean);
+   procedure Clear_Software_Interrupt (Hart_ID : Unsigned_64);
+   function Read_Software_Interrupt (Hart_ID : Unsigned_64) return Boolean;
 
-   procedure Clear_Software_Interrupt (Hart_ID : Unsigned_64)
-      with Inline,
-           Pre  => Hart_ID >= 0,
-           Post => True;
+   --  Timer management.
+   function Get_MTime return Unsigned_64;
+   procedure Set_Timer_Compare (Hart_ID : Unsigned_64; Time : Unsigned_64);
+   function Get_Timer_Compare (Hart_ID : Unsigned_64) return Unsigned_64;
 
-   function Read_Software_Interrupt (Hart_ID : Unsigned_64) return Boolean
-      with Inline,
-           Pre  => Hart_ID >= 0,
-           Post => True;
-
-   ------------------------------------------------------------------------------
-   --  Timer Management
-   ------------------------------------------------------------------------------
-   function Get_MTime return Unsigned_64
-      with Inline,
-           Post => True;
-
-   procedure Set_Timer_Compare (Hart_ID : Unsigned_64; Time : Unsigned_64)
-      with Inline,
-           Pre  => Hart_ID >= 0,
-           Post => Get_Timer_Compare(Hart_ID) = Time;
-
-   function Get_Timer_Compare (Hart_ID : Unsigned_64) return Unsigned_64
-      with Inline,
-           Pre  => Hart_ID >= 0,
-           Post => True;
-
-   ------------------------------------------------------------------------------
-   --  Memory Barrier
-   ------------------------------------------------------------------------------
-   procedure Memory_Barrier with Inline;
+   --  Memory barrier.
+   procedure Memory_Barrier;
 
 end Arch.CLINT;
-   
